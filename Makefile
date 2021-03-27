775b93245d3e059a81b3cec55cb323f64e9572f1
yves <yves.dufournaud@gmail.com>
2021-03-27 19:04:58 +0100

    [input] - list & detect device type

--- Makefile
+++ Makefile
@@ -1,22 +1,23 @@
 CXX = arm-linux-gnueabihf-g++
 # DO NOT USE -static => it prevent rm2fb-client patching
-CFLAGS ?= -fPIC -g -std=gnu++17 -Werror=return-type
+CFLAGS ?= -fPIC -g --std=gnu++17 -Werror=return-type
 
 DEVICE_IP ?= '10.11.99.1'
 DEVICE_HOST ?= root@$(DEVICE_IP)
 
+ 
 CAIRO ?= /home/yves/DEV/reMarkable/cairo
 CAIROLIB ?= $(CAIRO)/build-arm
 
-LDFLAGS ?= -L $(CAIROLIB)/src/
+LDFLAGS ?= -L $(CAIROLIB)/src/ -lstdc++fs
 
 
 core:
 
 build: core
 	$(CXX)  $(CFLAGS) core/test/core_fb_test.cc   -o fb_test
-	$(CXX)  $(CFLAGS) core/test/input_test.cc     -o input_test
-	$(CXX)  $(CFLAGS) -I $(CAIRO)/src -I $(CAIROLIB)/src $(LDFLAGS) core/test/animation_test.cc -l cairo -o animation_test
+	$(CXX)  $(CFLAGS) core/test/input_test.cc     -o input_test $(LDFLAGS) 
+#	$(CXX)  $(CFLAGS) -I $(CAIRO)/src -I $(CAIROLIB)/src core/test/animation_test.cc -l cairo -o animation_test $(LDFLAGS) 
 
 deploy-lib:
 	scp $(CAIROLIB)/src/libcairo.so.2 $(DEVICE_HOST):
@@ -24,10 +25,10 @@ deploy-lib:
 
 deploy: build
 	scp ./animation_test $(DEVICE_HOST):
-	ssh $(DEVICE_HOST) 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. /opt/bin/rm2fb-client ./animation_test'
+	ssh -t $(DEVICE_HOST) 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. /opt/bin/rm2fb-client ./animation_test'
 
 
 input_test: build
 	scp ./input_test $(DEVICE_HOST):
-	ssh $(DEVICE_HOST) './input_test'
+	ssh -t $(DEVICE_HOST) './input_test'
 
--- core/test/input_test.cc
+++ core/test/input_test.cc
@@ -8,6 +8,54 @@
 
 using namespace std;
 
+#include <filesystem>
+namespace fs = std::filesystem;
+
+
+// from https://github.com/rmkit-dev/rmkit/blob/master/src/rmkit/input/device_id.cpy
+// glommed from evtest.c
+#define BITS_PER_LONG (sizeof(long) * 8)
+#define NBITS(x) ((((x)-1)/BITS_PER_LONG)+1)
+#define OFF(x)  ((x)%BITS_PER_LONG)
+#define BIT(x)  (1UL<<OFF(x))
+#define LONG(x) ((x)/BITS_PER_LONG)
+#define test_bit(bit, array)    ((array[LONG(bit)] >> OFF(bit)) & 1)
+
+
+auto check_bit_set(int fd, int type, int i) {
+    unsigned long bit[NBITS(KEY_MAX)];
+    ioctl(fd, EVIOCGBIT(type, KEY_MAX), bit);
+    return test_bit(i, bit);
+}
+
+std::string id_by_capabilities(int fd) {
+    int version;
+    // if we can't get version of the fd, its invalid
+    if( ioctl(fd, EVIOCGVERSION, &version))
+        return "INVALID";
+
+    unsigned long bit[EV_MAX];
+    ioctl(fd, EVIOCGBIT(0, EV_MAX), bit);
+    if( test_bit(EV_KEY, bit)) {
+        if( check_bit_set(fd, EV_KEY, BTN_STYLUS) 
+            && test_bit(EV_ABS, bit))
+            return "STYLUS";
+
+        if( check_bit_set(fd, EV_KEY, KEY_POWER))
+            return "BUTTONS";
+
+        if( test_bit(EV_REL, bit)
+            && check_bit_set(fd, EV_ABS, ABS_MT_SLOT))
+            return "TOUCH";
+    }
+
+    return "UNKNOWN";
+}
+// end from https://github.com/rmkit-dev/rmkit/blob/master/src/rmkit/input/device_id.cpy
+
+
+#define SYM(element) [element] = #element
+
 array<const char*, EV_CNT> ev_type = {};
 array<const char*, SYN_CNT> syn_code = {};
 array<const char*, ABS_CNT> abs_code = {};
@@ -23,10 +71,24 @@ auto to_s( int value, const array<T,N>& label, const string& type) {
 }
 
 class Input {
+    vector<int> device;
     int pen;
 
 public:
-    Input( const char* pen_device = "/dev/input/event1" ) {
+    Input( const char* pen_device = "/dev/input/event0" ) {
+
+        cerr << "scanning input devices" << endl;
+        for(auto& p: fs::directory_iterator("/dev/input")) {
+            if( !p.is_symlink() && p.is_character_file() ) {
+                int dev = ::open( p.path().c_str(), O_RDONLY);
+
+                std::cerr << p.path() << " " << id_by_capabilities(dev) << endl;
+
+                device.push_back( dev);
+            }
+        }
+        cerr << "end" << endl;
+
         pen = ::open( pen_device, O_RDONLY);
 
         // define event name
@@ -38,6 +100,7 @@ public:
         syn_code[SYN_REPORT] = "SYN_REPORT";
 
         // define ABS code
+        // pen
         abs_code[ABS_X] = "ABS_X";
         abs_code[ABS_Y] = "ABS_Y";
         abs_code[ABS_DISTANCE] = "ABS_DISTANCE";
@@ -48,6 +111,24 @@ public:
         // btn code
         btn_code[BTN_TOOL_PEN]    = "BTN_TOOL_PEN";
         btn_code[BTN_TOOL_RUBBER] = "BTN_TOOL_RUBBER";
+
+        //
+        btn_code[KEY_POWER] = "KEY_POWER";
+
+        // multitouch
+        abs_code[ABS_MT_SLOT] = "ABS_MT_SLOT";
+        abs_code[ABS_MT_TOUCH_MAJOR] = "ABS_MT_TOUCH_MAJOR";
+        abs_code[ABS_MT_TOUCH_MINOR] = "ABS_MT_TOUCH_MINOR";
+        abs_code[ABS_MT_WIDTH_MAJOR] = "ABS_MT_WIDTH_MAJOR";
+        abs_code[ABS_MT_WIDTH_MINOR] = "ABS_MT_WIDTH_MINOR";
+        abs_code[ABS_MT_ORIENTATION] = "ABS_MT_ORIENTATION";
+        abs_code[ABS_MT_POSITION_X] = "ABS_MT_POSITION_X";
+        abs_code[ABS_MT_POSITION_Y] = "ABS_MT_POSITION_Y";
+        abs_code[ABS_MT_TOOL_TYPE] = "ABS_MT_TOOL_TYPE";
+        abs_code[ABS_MT_BLOB_ID] = "ABS_MT_BLOB_ID";
+        abs_code[ABS_MT_TRACKING_ID] = "ABS_MT_TRACKING_ID";
+        abs_code[ABS_MT_PRESSURE] = "ABS_MT_PRESSURE";
+
 
         for(;;) {
             input_event event;
@@ -64,11 +145,11 @@ public:
                     cerr << " " << ::to_s( event.code, syn_code, "code");
                     break;                
 
-                case EV_KEY:
+                case EV_KEY:       // hardware button + pen type
                     cerr << " " << ::to_s( event.code, btn_code, "code");
                     break;
 
-                case EV_ABS:
+                case EV_ABS:        // pen + multitouch
                     cerr << " " << ::to_s( event.code, abs_code, "code");
                     break;
             }
@@ -79,7 +160,6 @@ public:
 
         }
     }
-
 };
 
 int main(int argc,char** argv) {
